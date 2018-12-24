ec20c1dfdf1bafb236c750bc45d8b04fa72d5d29
Your Name <you@example.com>
2018-12-24 16:50:06 +0530

    search commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+	<link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
+	<title>Anush Here</title>
+	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="css/search.css">
+  <script src="js/search.js"></script>
+
+  </head>
+<body >
+<!-- Search Form -->
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="index.php"><span><img src="images/test.png" width="20px";height="20px">Anush G</span></a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="about.php">About Developer</a></li>
+      <li><a href="liniks.html">social links</a></li>
+      <li><a href="#">version</a></li>
+    </ul>
+  </div>
+</nav>
+
+<h1 class="welcome-text">Hi Anush!</h1> 
+<form id="labnol" method="get" target="blank" action="https://www.google.com/search">
+  <div class="speech">
+     
+    <input type="text" name="q" id="transcript" placeholder="Type here"/>
+    <img onclick="startDictation()" src="images/test.png" />
+  </div>
+</form>
+ 
+
+</body>
+
+<!-- <footer class="footer">I am built by Anush Ganiga</footer>
+ --></html>
